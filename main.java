6b2fb217127fe97d13c0f3207316036dc9000acb
rasko1 <oophn.88@yandex.ru>
2023-02-09 18:57:16 +0300

    first

---
+++ main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        int price = 32232;
+        int miles = price / 20;
+        System.out.println("За " + price + "р. "+ "Вам начислят " + miles + " миль");
+    }
+}
+
